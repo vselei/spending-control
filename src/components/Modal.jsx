465c3f3edf3cf8c86169badf8ef2965fb0f8dbd9
Victor Seleimend <vsennaselei@gmail.com>
2023-01-29 11:20:27 -0300

    Formulário de adição de despesas

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,6 +17,33 @@ const Modal = ({ setModal, animateModal, setAnimateModal }) => {
 
       <form className={`form ${animateModal ? 'animate' : 'close'}`}>
         <legend>Novo gasto</legend>
+
+        <div className="field">
+          <label htmlFor="name">Nome do Gasto</label>
+          <input id="name" type="text" placeholder="Adicione o nome do gasto" />
+        </div>
+        <div className="field">
+          <label htmlFor="qty">Quantidade</label>
+          <input
+            id="qty"
+            type="number"
+            placeholder="Adicione o quanto foi gasto: ex. 300"
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="filter">Quantidade</label>
+          <select id="filter">
+            <option value="">-- Selecione --</option>
+            <option value="economy">Economia</option>
+            <option value="food">Comida</option>
+            <option value="house">Casa</option>
+            <option value="miscellaneous">Diverso</option>
+            <option value="lounge">Lazer</option>
+            <option value="health">Saúde</option>
+            <option value="subscriptions">Assinatura</option>
+          </select>
+        </div>
+        <input type="submit" value="Adicionar Despesa" />
       </form>
     </div>
   );
